b9d0af0a7e62a3c9bfb3d445a097a2a4d19fc468
3rd-Eden <info@3rd-Eden.com>
2014-08-13 11:51:50 +0200

    [fix] Attempt to discover if node is in the path #12

--- install.js
+++ install.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var path = require('path')
+var which = require('which')
+  , path = require('path')
   , fs = require('fs');
 
 //
@@ -11,7 +12,7 @@ var existsSync = fs.existsSync || path.existsSync;
 //
 // Our own pre-commit hook runner.
 //
-var hook = fs.readFileSync('./hook');
+var hook = fs.readFileSync('./hook', 'utf-8');
 
 //
 // The root of repository.
@@ -29,7 +30,9 @@ var git = path.resolve(root, '.git')
 // Check if we are in a git repository so we can bail out early when this is not
 // the case.
 //
-if (!existsSync(git) || !fs.lstatSync(git).isDirectory()) return;
+if (!existsSync(git) || !fs.lstatSync(git).isDirectory()) {
+  return console.error('pre-commit: No valid git or hooks directory found.');
+}
 
 //
 // Create a hooks directory if it's missing.
@@ -37,13 +40,24 @@ if (!existsSync(git) || !fs.lstatSync(git).isDirectory()) return;
 if (!existsSync(hooks)) fs.mkdirSync(hooks);
 
 //
-// If there's an existing `pre-commit` hook we want to back it up instead of
-// overriding it and losing it completely
+// We cannot assume that `node` is in the $PATH as `env node` doesn't work well
+// with GUI clients according to observing/pre-commit#12
 //
-if (
-     existsSync(precommit)
-  && fs.readFileSync(precommit).toString('utf8') !== hook.toString('utf8')
-) {
+try {
+  require('which').sync('node');
+} catch (e) {
+  hook = hook.replace('#!/usr/bin/env node',
+    existsSync('/usr/local/bin/node')
+    ? '/usr/local/bin/node'
+    : process.execPath
+  );
+}
+
+//
+// If there's an existing `pre-commit` hook we want to back it up instead of
+// overriding it and losing it completely.
+//
+if (existsSync(precommit) && fs.readFileSync(precommit, 'uf8') !== hook) {
   console.log('');
   console.log('pre-commit: Detected an existing git pre-commit hook');
   fs.writeFileSync(precommit +'.old', fs.readFileSync(precommit));
